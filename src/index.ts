efeda7ef605406865a58bbbfec31cc4c0322b297
Eric M. Dantas <ericdantas0@gmail.com>
2020-06-22 15:48:36 -0300

    Better organization

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('a')
+import { MyInterface } from "typings-package2-issue"
+import { SomethingElse } from "typings-package2-issue"
+import { MyInterface } from "typings-package1-issue"
+
+class AnotherOne implements MyInterface {
+    doSomething():string {
+        return "ok"
+    }
+} 
+
+new AnotherOne().doSomething()
+new SomethingElse().doSomething()
